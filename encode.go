0b8cb9fd154b56a0554504e768a2caa31aa01a91
Li-giegie <1261930106@qq.com>
2024-02-03 23:30:49 +0800

    更新：更新编解码实现，增加EncodeFaster快速编码实现、CountLength计算结构长度.

--- encode.go
+++ encode.go
@@ -10,7 +10,8 @@ import (
 
 var BufferSize = 128
 
-func EncodeV2(buf []byte, args ...interface{}) ([]byte, error) {
+// EncodeFaster 在测试文件encode_decode_test.go中查看使用例子
+func EncodeFaster(buf []byte, args ...interface{}) ([]byte, error) {
 	for i := 0; i < len(args); i++ {
 		switch v := args[i].(type) {
 		case string:
@@ -59,189 +60,16 @@ func EncodeV2(buf []byte, args ...interface{}) ([]byte, error) {
 	return buf, nil
 }
 
-// Encode 将go中的基本值类型进行编码，编码的参数和解码的参数顺序必须一致，只有在传递的类型不支持时会返回错误，其他情况不会，注意这一步并不打包返回的切片
-//
-//	func TestEncode(t *testing.T) {
-//		var s struct{
-//			A int
-//			B string
-//			C bool
-//		}
-//
-//		buf ,err := Encode(s.A,s.B,s.C)
-//		if err != nil {
-//			return
-//		}
-//		fmt.Println(buf)
-//	}
+// Encode 在测试文件encode_decode_test.go中查看使用例子
 func Encode(args ...interface{}) ([]byte, error) {
-	var buf = make([]byte, 0, BufferSize)
-	for i := 0; i < len(args); i++ {
-		switch v := args[i].(type) {
-		case string:
-			var hl = make([]byte, 4)
-			binary.LittleEndian.PutUint32(hl, uint32(len(v)))
-			var tmpBuffer []byte
-			*(*string)(unsafe.Pointer(&tmpBuffer)) = v
-			*(*int)(unsafe.Pointer(uintptr(unsafe.Pointer(&tmpBuffer)) + 2*unsafe.Sizeof(&tmpBuffer))) = len(v)
-			buf = append(buf, hl...)
-			buf = append(buf, tmpBuffer...)
-		case int:
-			tmpBuffer := make([]byte, binary.MaxVarintLen64)
-			binary.PutVarint(tmpBuffer, int64(v))
-			buf = append(buf, tmpBuffer...)
-		case []byte:
-			var hl = make([]byte, 4)
-			binary.LittleEndian.PutUint32(hl, uint32(len(v)))
-			buf = append(buf, hl...)
-			buf = append(buf, v...)
-		case int8:
-			buf = append(buf, uint8(v))
-		case int16:
-			tmpBuffer := make([]byte, binary.MaxVarintLen16)
-			binary.PutVarint(tmpBuffer, int64(v))
-			buf = append(buf, tmpBuffer...)
-		case int32:
-			tmpBuffer := make([]byte, binary.MaxVarintLen32)
-			binary.PutVarint(tmpBuffer, int64(v))
-			buf = append(buf, tmpBuffer...)
-		case int64:
-			tmpBuffer := make([]byte, binary.MaxVarintLen64)
-			binary.PutVarint(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-		case uint:
-			tmpBuffer := make([]byte, 8)
-			binary.LittleEndian.PutUint64(tmpBuffer, uint64(v))
-			buf = append(buf, tmpBuffer...)
-		case uint8:
-			buf = append(buf, []byte{v}...)
-		case uint16:
-			tmpBuffer := make([]byte, 2)
-			binary.LittleEndian.PutUint16(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-		case uint32:
-			tmpBuffer := make([]byte, 4)
-			binary.LittleEndian.PutUint32(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-		case uint64:
-			tmpBuffer := make([]byte, 8)
-			binary.LittleEndian.PutUint64(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-		case float32:
-			tmpBuffer := make([]byte, 4)
-			binary.LittleEndian.PutUint32(tmpBuffer, math.Float32bits(v))
-			buf = append(buf, tmpBuffer...)
-		case float64:
-			tmpBuffer := make([]byte, 8)
-			binary.LittleEndian.PutUint64(tmpBuffer, math.Float64bits(v))
-			buf = append(buf, tmpBuffer...)
-		case bool:
-			if v {
-				buf = append(buf, 1)
-			} else {
-				buf = append(buf, 0)
-			}
-		default:
-			return nil, encodeError(i)
-		}
+	buf, err := EncodeFaster(make([]byte, 0, BufferSize), args...)
+	if err == nil {
+		BufferSize = len(buf)
 	}
-	BufferSize = len(buf)
-	return buf, nil
+	return buf, err
 }
 
-// 同Encode相同，不同的是每当解码一个字段时会记录其长度，方便在内容发生变化后不重新编码的情况下修改
-func EncodeWithLenByteItem(args ...interface{}) (buf []byte, itemLen []int32, err error) {
-	buf = make([]byte, 0, BufferSize)
-	itemLen = make([]int32, 0, len(args))
-	for index, arg := range args {
-		switch v := arg.(type) {
-		case string:
-			var hl = make([]byte, 4)
-			binary.LittleEndian.PutUint32(hl, uint32(len(v)))
-			var tmpBuffer []byte
-			*(*string)(unsafe.Pointer(&tmpBuffer)) = v
-			*(*int)(unsafe.Pointer(uintptr(unsafe.Pointer(&tmpBuffer)) + 2*unsafe.Sizeof(&tmpBuffer))) = len(v)
-			buf = append(buf, hl...)
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, int32(4+len(v)))
-		case []byte:
-			var hl = make([]byte, 4)
-			binary.LittleEndian.PutUint32(hl, uint32(len(v)))
-			buf = append(buf, hl...)
-			buf = append(buf, v...)
-			itemLen = append(itemLen, int32(4+len(v)))
-		case int:
-			tmpBuffer := make([]byte, binary.MaxVarintLen64)
-			binary.PutVarint(tmpBuffer, int64(v))
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, int32(binary.MaxVarintLen64))
-		case int8:
-			buf = append(buf, uint8(v))
-			itemLen = append(itemLen, 1)
-		case int16:
-			tmpBuffer := make([]byte, binary.MaxVarintLen16)
-			binary.PutVarint(tmpBuffer, int64(v))
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, int32(binary.MaxVarintLen16))
-		case int32:
-			tmpBuffer := make([]byte, binary.MaxVarintLen32)
-			binary.PutVarint(tmpBuffer, int64(v))
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, int32(binary.MaxVarintLen32))
-		case int64:
-			tmpBuffer := make([]byte, binary.MaxVarintLen64)
-			binary.PutVarint(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, int32(binary.MaxVarintLen64))
-		case uint:
-			tmpBuffer := make([]byte, 8)
-			binary.LittleEndian.PutUint64(tmpBuffer, uint64(v))
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, 8)
-		case uint8:
-			buf = append(buf, []byte{v}...)
-			itemLen = append(itemLen, 1)
-		case uint16:
-			tmpBuffer := make([]byte, 2)
-			binary.LittleEndian.PutUint16(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, 2)
-		case uint32:
-			tmpBuffer := make([]byte, 4)
-			binary.LittleEndian.PutUint32(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, 4)
-		case uint64:
-			tmpBuffer := make([]byte, 8)
-			binary.LittleEndian.PutUint64(tmpBuffer, v)
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, 8)
-		case float32:
-			tmpBuffer := make([]byte, 4)
-			binary.LittleEndian.PutUint32(tmpBuffer, math.Float32bits(v))
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, 4)
-		case float64:
-			tmpBuffer := make([]byte, 8)
-			binary.LittleEndian.PutUint64(tmpBuffer, math.Float64bits(v))
-			buf = append(buf, tmpBuffer...)
-			itemLen = append(itemLen, 8)
-		case bool:
-			if v {
-				buf = append(buf, 1)
-			} else {
-				buf = append(buf, 0)
-			}
-			itemLen = append(itemLen, 1)
-		default:
-			return nil, nil, encodeError(index)
-		}
-	}
-
-	return buf, itemLen, nil
-}
-
-// EncodeSlice 将入参切片编码：支持列表[[]uint32]
+// EncodeSlice 在测试文件encode_decode_test.go中查看使用例子
 func EncodeSlice(slice ...interface{}) ([]byte, error) {
 	var buf = make([]byte, 0, BufferSize)
 	for index, item := range slice {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Li-giegie/go-jeans
 go 1.19
 
 require (
+	github.com/bxcodec/faker/v3 v3.8.1
 	github.com/golang/protobuf v1.5.0
 	github.com/vmihailenco/msgpack/v5 v5.4.0
 	google.golang.org/protobuf v1.30.0
